72b92bd3c92c5227250ed08a4599f7a5894457e6
gourav <heychoudhary.gourav@gmail.com>
2025-08-10 15:48:16 +0530

    make responsive

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
   let currentsong = new Audio();
 
+function convertToMinutesAndSeconds(totalSeconds) {
+  totalSeconds = Math.floor(totalSeconds); // 
+
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(seconds).padStart(2, '0');
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+
+
+
 async function fetchSongs() {
   try {
     const res = await fetch("http://127.0.0.1:3000/songs/");
@@ -37,7 +52,6 @@ const playmusic = (track)=>{
       .replace(/\s+/g, " ")
       .replaceAll("%20", " ");
   document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
-  
 }
 async function main() {
 
@@ -80,11 +94,45 @@ async function main() {
   play.addEventListener("click",()=>{
     if(currentsong.paused){
       currentsong.play()
+      play.src = "Assest/Images/pause.svg";
     }
     else{
-      currentsong.pause()
+      currentsong.pause() 
+      play.src = "Assest/Images/play.svg";
     }
   })
+  currentsong.addEventListener("timeupdate",()=>{
+    console.log(currentsong.currentTime, currentsong.duration)
+    document.querySelector(".songtime").innerHTML =`${convertToMinutesAndSeconds(currentsong.currentTime)} / ${convertToMinutesAndSeconds(currentsong.duration)}`
+    document.querySelector(".circle").style.left = (currentsong.currentTime/currentsong.duration)*100 +"%"
+  })
+
+  document.querySelector(".seekbar").addEventListener("click",e=>{
+    let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+    document.querySelector(".circle").style.left=percent + "%";
+    currentsong.currentTime = ((currentsong.duration)* percent)/100;
+  })
+const leftPanel = document.querySelector(".left");
+document.querySelector(".hamburger").addEventListener("click", () => {
+  if (leftPanel.style.left === "0%") {
+    leftPanel.style.left = "-100%";
+  } else {
+    leftPanel.style.left = "0%";
+  }
+});
+document.addEventListener("click", (event) => {
+  const leftPanel = document.querySelector(".left");
+  const hamburger = document.querySelector(".hamburger");
+
+ 
+  if (!leftPanel.contains(event.target) && !hamburger.contains(event.target)) {
+    leftPanel.style.left = "-100%";
+  }
+else if (event.target.src && event.target.src.includes("logo.svg")) {
+  leftPanel.style.left = "-100%";
+}
+});
+
 
 }
 main()
